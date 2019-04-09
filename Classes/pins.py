19aa6d30250e6a3293c08e2fcc37ee3a833fc9dd
Uriel Sussman <urielsussman@gmail.com>
2019-04-09 15:16:16 -0400

    Added pin module

---
+++ Classes/pins.py
@@ -0,0 +1,10 @@
+motors = (
+    (speed1, forward1, backward1),
+    (speed2, forward2, backward2),
+    (speed3, forward3, backward3),
+    (speed4, forward4, backward4)
+)
+
+dribbler = (speed, forward, backward)
+kicker = (on)
+line_sensors = (in1, in2, in3, in4)
