55d0b50ed25bfd452ae307dfa8d20da2b2e7765c
Uriel Sussman <urielsussman@gmail.com>
2019-04-09 16:08:14 -0400

    Added additional classes and pins

--- Classes/__init__.py
+++ Classes/__init__.py
@@ -1,19 +1,25 @@
 from direction import Compass
 from driver import Driver
+from kicker import Kicker
+from dribbler import Dribbler
 from finder import Camera
 from ir_sensors import LineSensor
+import pins
 
 
 class Director:
     def __init__(self):
         self.eyes = Camera()
         self.legs = Driver()
-        self.lookout = LineSensor()
         self.navigator = Compass()
-    
+        self.lookout = LineSensor(*pins.line_sensors)
+        self.kicker = Kicker(pins.kicker)
+        self.dribbler = Dribbler(*pins.dribbler)
+
     def direct(self):
         while True:
             ...
+
 
 if __name__ == '__main__':
     the_director = Director()
